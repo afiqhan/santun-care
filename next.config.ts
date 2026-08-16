c777a164dcbf5ce74f24e03fa23b346b42d49953
afiq <afiqhanif12345@gmail.com>
2026-08-17 00:32:46 +0800

    Initial Commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+const repoName = "santun-care"; // Gantikan jika nama repositori GitHub anda berbeza
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // Menjana fail HTML statik
+  images: {
+    unoptimized: true, // Diperlukan untuk persekitaran statik GitHub Pages
+  },
+  basePath: isGithubActions ? `/${repoName}` : "",
+  assetPrefix: isGithubActions ? `/${repoName}/` : "",
 };
 
 export default nextConfig;
